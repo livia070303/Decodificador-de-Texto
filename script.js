499e45a2956c98d1243e6d5b283f35bd40e7db0b
Livia <liviarooly.com>
2023-05-22 21:09:22 -0300

    Adicionando validação

--- script.js
+++ script.js
@@ -9,31 +9,49 @@ let resultado = document.getElementById("resultado");
 
 function Criptografar() {
     let palavra = document.getElementById("palavra").value;
+    if(EhMinusculo(palavra) && SemAcento(palavra)) {
+        var palavraCriptografada = palavra.replaceAll("e", "enter");
+        palavraCriptografada = palavraCriptografada.replaceAll("i", "imes");
+        palavraCriptografada = palavraCriptografada.replaceAll("a", "ai");
+        palavraCriptografada = palavraCriptografada.replaceAll("o", "ober");
+        palavraCriptografada = palavraCriptografada.replaceAll("u", "ufat");
 
-    var palavraCriptografada = palavra.replaceAll("e", "enter");
-    palavraCriptografada = palavraCriptografada.replaceAll("i", "imes");
-    palavraCriptografada = palavraCriptografada.replaceAll("a", "ai");
-    palavraCriptografada = palavraCriptografada.replaceAll("o", "ober");
-    palavraCriptografada = palavraCriptografada.replaceAll("u", "ufat");
+        resultado.value = palavraCriptografada;
+        document.getElementById("bloco-retorno").style.display = "block";
+        document.getElementById("bloco-estatico").style.display = "none";
+        document.getElementById("copiado").style.background = "#EFF1FA";
+    } else{
+        alert("Somente letras minuscúlas e sem acento!")
+    }
 
-    resultado.value = palavraCriptografada;
-    document.getElementById("bloco-retorno").style.display = "block";
-    document.getElementById("bloco-estatico").style.display = "none";
-    document.getElementById("copiado").style.background = "#EFF1FA";
 }
 
 function Descriptografar() {
+    var palavra = document.getElementById("palavra").value;
+    if(EhMinusculo(palavra) && SemAcento(palavra)) {
+        var palavraDescriptografada = palavra.replaceAll("ufat", "u");
+        palavraDescriptografada = palavraDescriptografada.replaceAll("ober", "o");
+        palavraDescriptografada = palavraDescriptografada.replaceAll("ai", "a");
+        palavraDescriptografada = palavraDescriptografada.replaceAll("imes", "i");
+        palavraDescriptografada = palavraDescriptografada.replaceAll("enter", "e");
 
-    var palavraDescriptografada = document.getElementById("palavra").value.replaceAll("ufat", "u");
-    palavraDescriptografada = palavraDescriptografada.replaceAll("ober", "o");
-    palavraDescriptografada = palavraDescriptografada.replaceAll("ai", "a");
-    palavraDescriptografada = palavraDescriptografada.replaceAll("imes", "i");
-    palavraDescriptografada = palavraDescriptografada.replaceAll("enter", "e");
+        resultado.value = palavraDescriptografada;
+        document.getElementById("bloco-retorno").style.display = "block";
+        document.getElementById("bloco-estatico").style.display = "none";
+        document.getElementById("copiado").style.background = "#EFF1FA";
 
-    resultado.value = palavraDescriptografada;
-    document.getElementById("bloco-retorno").style.display = "block";
-    document.getElementById("bloco-estatico").style.display = "none";
-    document.getElementById("copiado").style.background = "#EFF1FA";
+    } else{
+        alert("Somente letras minúsculas e sem acento!");
+    }
+
+}
+function EhMinusculo(palavra) {
+    var minusculo = palavra.toLowerCase();
+    return palavra === minusculo;
+}
+function SemAcento(palavra) {
+    var semAcento = palavra.normalize("NFD").replace(/[\u0300-\u036f]/g,"");
+    return semAcento === palavra;
 
 }
 
